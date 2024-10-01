6d9be0b5ec1ee4d22f3552a1c4ed8c3ae9642ad9
kalevlill <lill.kalev@gmail.com>
2024-10-01 15:13:34 +0200

    Aufgabe 1 erledigt

---
+++ Aufgabe1.py
@@ -0,0 +1,13 @@
+# Definieren der Variablen
+
+first_name = "Kalev" # Vorname als String eingefuegt
+last_name = "Lill" # Nachname als String eingefuegt 
+birth_year = 1996 # Geburtsjahr als Ganzzahl eingefuegt
+has_license = True # Fuehrerschein status als boolean
+
+# Ausgabe der Variablen
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Birth year:", birth_year)
+print("License status:", has_license)
